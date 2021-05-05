f537c80bcec41a866ca90db009122c8e3d5a537c
Cindy Lam <cindybinh444@gmail.com>
2021-05-04 20:54:43 -0400

    Added Lesson 3 SolarSystem Assets and Rough Script Draft

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    public Vector3 locationNumber; //bucket of information that you have to fill
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start has been called hahaaaaaa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Start has been called hahaha");
+
+    }
+}
